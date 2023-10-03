b48c29fbc263a37cd9413cdaf2fbd8a160c96387
chien67 <chien672001@gmail.com>
2023-10-03 22:59:38 +0700

    add layout

--- CRUD_UsingSQL/MVC/App_Start/BundleConfig.cs
+++ CRUD_UsingSQL/MVC/App_Start/BundleConfig.cs
@@ -8,23 +8,51 @@ namespace MVC
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Bundles/css")
+                .Include("~/Content/css/bootstrap.min.css")
+                .Include("~/Content/css/bootstrap-select.css")
+                .Include("~/Content/css/bootstrap-datepicker3.css")
+                .Include("~/Content/css/font-awesome.min.css")
+                .Include("~/Content/css/icheck/blue.min.css")
+                .Include("~/Content/css/AdminLTE.css")
+                .Include("~/Content/css/skins/skin-blue.css"));
+
+            bundles.Add(new ScriptBundle("~/Bundles/js")
+                .Include("~/Content/js/plugins/jquery/jquery-3.4.1.js")
+                .Include("~/Content/js/plugins/bootstrap/bootstrap.js")
+                .Include("~/Content/js/plugins/fastclick/fastclick.js")
+                .Include("~/Content/js/plugins/slimscroll/jquery.slimscroll.js")
+                .Include("~/Content/js/plugins/bootstrap-select/bootstrap-select.js")
+                .Include("~/Content/js/plugins/moment/moment.js")
+                .Include("~/Content/js/plugins/datepicker/bootstrap-datepicker.js")
+                .Include("~/Content/js/plugins/icheck/icheck.js")
+                .Include("~/Content/js/plugins/validator/validator.js")
+                .Include("~/Content/js/plugins/inputmask/jquery.inputmask.bundle.js")
+                .Include("~/Content/js/adminlte.js")
+                .Include("~/Content/js/init.js"));
+
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
+            BundleTable.EnableOptimizations = true;
+#endif
         }
     }
 }
